dc1a54a763601c9fb6d302b3ead111842afa5040
Johannes Meyer <meyer@fsr.tu-darmstadt.de>
2012-09-17 13:20:38 +0000

    hector_uav_msgs: merged changeset r585 from /branches/fuerte
    git-svn-id: https://tu-darmstadt-ros-pkg.googlecode.com/svn/trunk/hector_quadrotor@600 362d0299-bcd8-93f0-de3f-00335e8b5ee8

---
+++ hector_uav_msgs/include/hector_uav_msgs/ControlSource.h
@@ -0,0 +1,56 @@
+//=================================================================================================
+// Copyright (c) 2012, Johannes Meyer, TU Darmstadt
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Flight Systems and Automatic Control group,
+//       TU Darmstadt, nor the names of its contributors may be used to
+//       endorse or promote products derived from this software without
+//       specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//=================================================================================================
+
+#ifndef HECTOR_UAV_MSGS_CONTROLSOURCE_H
+#define HECTOR_UAV_MSGS_CONTROLSOURCE_H
+
+namespace hector_uav_msgs
+{
+  typedef uint8_t ControlSource;
+
+  enum {
+    CONTROL_AUTONOMOUS = 0,
+    CONTROL_REMOTE = 1,
+    CONTROL_JOYSTICK = 2
+  };
+
+  template <typename InStream>
+  static inline InStream& operator>>(InStream& in, ControlSource& value) {
+    int temp;
+    in >> temp;
+    value = static_cast<ControlSource>(temp);
+    return in;
+  }
+
+  template <typename OutStream>
+  static inline OutStream& operator<<(OutStream& out, const ControlSource& value) {
+    return out << static_cast<int>(value);
+  }
+}
+
+#endif // HECTOR_UAV_MSGS_CONTROLSOURCE_H
